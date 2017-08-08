47d315d06d0479cb62549536ea3771517636230f
Wilson <sweekson@gmail.com>
2017-08-08 04:12:10 +0000

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 /* global $ */
-
-$(function () {
-  var $app = $('#app');
-  var $h1 = $('<h1>');
-  var heading = 'Hello, Node.js';
-  
-  $h1.text(heading);
-  $app.append($h1);
+var $item = $('<li class="list-group-item">');
+var $todos, $task, $app = $('#app');
+$app.on('click', '.btn-add', function () {
+  $todos.append($item.clone().append($task.val()));
+});
+$.when(
+  $.get('list.html'), $.get('create.html')
+)
+.then(function (list, create) {
+  $app.append(list[0], create[0]);
+  $todos = $('#todos'), $task = $('#task');
 });
